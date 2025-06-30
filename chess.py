dc712a123920d6d964ab2b250625bcd5d9829939
rokrerum <132296840+rokrerum@users.noreply.github.com>
2025-07-01 00:09:26 +0200

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,14 +1,53 @@
 class piece_moves:
-    def pawn_moves(self):
+    global posible_moves
+    
+    def pawn_moves(board, row, col):
+        posible_moves = []
+        if board[row][col][0] == "P":
+            if row == 6:
+                if len(board[row - 1][col]) == 0:
+                    posible_moves.append([row - 1, col])
+                    
+                    if len(board[row - 2][col]) == 0:
+                        posible_moves.append([row - 2, col])
+
+                    
+            else:
+                if board[row + 1][col] == " ":
+                    posible_moves.append([row - 1, col])
+            
+            if col + 1 < 8 and len(board[row - 1][col + 1]) > 0 and board[row - 1][col + 1][0].lower() == board[row - 1][col + 1][0]:
+                posible_moves.append([row - 1, col + 1])
+            
+            if col - 1 < 8 and len(board[row - 1][col - 1]) > 0 and board[row - 1][col - 1][0].lower() == board[row - 1][col - 1][0]:
+                posible_moves.append([row - 1, col - 1])
+                
+        return posible_moves
+    
+        
+    def rook_moves(board, row, col):
         pass
-    def rook_moves(self):
+    
+    
+    def knight_moves(board, row, col):
         pass
-    def knight_moves(self):
+    
+    
+    def bishop_moves(board, row, col):
         pass
-    def bishop_moves(self):
+    
+    
+    def queen_moves(board, row, col):
         pass
-    def queen_moves(self):
+    
+    
+    def king_moves(board, row, col):
         pass
-    def king_moves(self):
-        pass
+    
+    
+    def chec(self):
+        if True:
+            return True
+        else:
+            return False    
     
