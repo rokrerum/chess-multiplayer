3c2573a082e691a1c26f45ef188e1c8b03e7410c
rokrerum <132296840+rokrerum@users.noreply.github.com>
2025-07-07 13:48:47 +0200

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,7 +1,7 @@
 class piece_moves:
     global posible_moves
     
-    def pawn_moves(board, row, col):#to remake becouse of my bad code
+    def pawn_moves(board, row, col):
         posible_moves = []
         if board[row][col][0] == "P":
             if row == 6:
@@ -13,7 +13,6 @@ class piece_moves:
 
             else:
                 if len(board[row - 1][col]) <= 0:
-                    print("pole na planszy" + board[row - 1][col])
                     posible_moves.append([row - 1, col])
             
             if col + 1 < 8 and len(board[row - 1][col + 1]) > 0 and board[row - 1][col + 1][0].lower() == board[row - 1][col + 1][0]:
@@ -28,46 +27,28 @@ class piece_moves:
         return posible_moves
     
         
-    def rook_moves(board, row, col): #to remake becouse of my bad code
+    def rook_moves(board, row, col): 
         posible_moves = []
-        #verticle
+        moves = [
+            [(row - i, col) for i in range(1, row + 1)],
+            [(row + i, col) for i in range(1, 8 - row)],
+            [(row, col - i) for i in range(1, col + 1)],
+            [(row, col + i) for i in range(1, 8 - col)]
+            
+        ]
         if board[row][col][0] == "R":
-            for i in range(row - 1, -1, -1): 
-                if len(board[i][col]) > 0:
-                    if  board[i][col][0].isupper():
-                        break
-                    else:
-                        posible_moves.append([i, col])
-                        break
-                posible_moves.append([i, col])
-
-            for i in range(row + 1, 8):
-                if len(board[i][col]) > 0:
-                    if  board[i][col][0].isupper():
-                        break
-                    else:
-                        posible_moves.append([i, col])
-                        break
-                posible_moves.append([i,col])
-
-            #horizontal
-            for i in range(col - 1, -1, -1): 
-                if len(board[row][i]) > 0:
-                    if  board[row][i][0].isupper():
-                        break
-                    else:
-                        posible_moves.append([row, i])
-                        break
-                posible_moves.append([row, i])
-
-            for i in range(col + 1, 8): 
-                if len(board[row][i]) > 0:
-                    if  board[row][i][0].isupper():
-                        break
-                    else:
-                        posible_moves.append([row, i])
-                        break
-                posible_moves.append([row, i])
+            for i in moves:
+                for move in i:
+                    if 0 <= move[0] < 8 and 0 <= move[1] < 8:
+                        if len(board[move[0]][move[1]]) == 0:
+                            posible_moves.append(move)
+                            
+                        elif board[move[0]][move[1]][0].islower():
+                            posible_moves.append(move)
+                            break
+                        
+                        else:
+                            break
 
         return posible_moves
     
@@ -85,7 +66,6 @@ class piece_moves:
             (row - 1, col - 2)
         ]
         if board[row][col][0] == "N":
-            
             for move in moves:
                 if 0 <= move[0] < 8 and 0 <= move[1] < 8:
                     if len(board[move[0]][move[1]]) == 0 or board[move[0]][move[1]][0].islower():
@@ -94,20 +74,23 @@ class piece_moves:
         return posible_moves
     
     
-    def bishop_moves(board, row, col): #not working
+    def bishop_moves(board, row, col):
         posible_moves = []
         moves = [
-            [(row - i, col - i) for i in range(1, row)],
-            [(row - i, col + i) for i in range(row)],
-            [(row - i, col + i) for i in range(row + 1, 8)],
-            [(row - i, col + i) for i in range()],
+            [(row - i, col - i) for i in range(1, row + 1)],
+            [(row - i, col + i) for i in range(1, row + 1)],
+            [(row + i, col - i) for i in range(1, 8 - row)],
+            [(row + i, col + i) for i in range(1, 8 - row)]
         ]
         if(board[row][col][0] == "B"):
             for i in moves:
                 for move in i:
                     if 0 <= move[0] < 8 and 0 <= move[1] < 8:
-                        if len(board[move[0]][move[1]]) == 0 or board[move[0]][move[1]][0].islower():
+                        if len(board[move[0]][move[1]]) == 0:
                             posible_moves.append(move)
+                        elif board[move[0]][move[1]][0].islower():
+                            posible_moves.append(move)
+                            break
                         else:
                             break
                     else:
@@ -116,18 +99,90 @@ class piece_moves:
         return posible_moves
         
     
-    
     def queen_moves(board, row, col):
-        pass
+        posible_moves = []
+        moves = [
+            [(row - i, col) for i in range(1, row + 1)], #rook moves
+            [(row + i, col) for i in range(1, 8 - row)],
+            [(row, col - i) for i in range(1, col + 1)],
+            [(row, col + i) for i in range(1, 8 - col)],
+            [(row - i, col - i) for i in range(1, row + 1)], #bishop moves
+            [(row - i, col + i) for i in range(1, row + 1)],
+            [(row + i, col - i) for i in range(1, 8 - row)],
+            [(row + i, col + i) for i in range(1, 8 - row)]
+        ]
+        
+        if board[row][col][0] == "Q":
+            
+            for i in moves:
+                for move in i:
+                    if 0 <= move[0] < 8 and 0 <= move[1] < 8:
+                        if len(board[move[0]][move[1]]) == 0:
+                            posible_moves.append(move)
+                            
+                        elif board[move[0]][move[1]][0].islower():
+                            posible_moves.append(move)
+                            break
+                        
+                        else:
+                            break
+                        
+        return posible_moves
     
     
     def king_moves(board, row, col):
-        pass
+        posible_moves = []
+        moves = [
+        (row + 1, col - 1),
+        (row + 1, col),
+        (row + 1, col + 1),
+        (row, col - 1),
+        (row, col + 1),
+        (row - 1, col - 1),
+        (row - 1, col),
+        (row - 1, col + 1)
+        ]
+        
+        if board[row][col][0] == "K":
+            for move in moves:
+                if 0 <= move[0] < 8 and 0 <= move[1] < 8:
+                    if len(board[move[0]][move[1]]) == 0 or board[move[0]][move[1]][0].islower():
+                        posible_moves.append(move)
+    
+        return posible_moves
     
     
-    def chec(self):
-        if True:
-            return True
-        else:
-            return False    
+    def chec(self, board, row, col): #not working
+        posible_ataks = [
+            [(row - i, col) for i in range(1, row + 1)], #rook moves
+            [(row + i, col) for i in range(1, 8 - row)],
+            [(row, col - i) for i in range(1, col + 1)],
+            [(row, col + i) for i in range(1, 8 - col)],
+            [(row - i, col - i) for i in range(1, row + 1)], #bishop moves
+            [(row - i, col + i) for i in range(1, row + 1)],
+            [(row + i, col - i) for i in range(1, 8 - row)],
+            [(row + i, col + i) for i in range(1, 8 - row)],    
+            (row + 2, col + 1),
+            (row + 2, col - 1),
+            (row - 2, col + 1),
+            (row - 2, col - 1),
+            (row + 1, col + 2),
+            (row + 1, col - 2),
+            (row - 1, col + 2),
+            (row - 1, col - 2)
+        ]
+        for i in posible_ataks:
+            for move in i:
+                if 0 <= move[0] < 8 and 0 <= move[1] < 8:
+                    if len(board[move[0]][move[1]]) == 0:
+                        pass
+                    elif board[move[0]][move[1]][0].islower():
+                        pass
+                    else:
+                        pass
+                else:
+                    break
+            
+
+        
     
