eacbeb9c140e92bc5df9740d4d1e11cca9dd1862
rokrerum <132296840+rokrerum@users.noreply.github.com>
2025-08-10 00:14:44 +0200

    Update chess_ai.py
    small improvements and fixes

--- chess_ai.py
+++ chess_ai.py
@@ -7,17 +7,16 @@ class AI:
         
     
     def ai(self, board, turn, my_color, en_passant, castling):
-        move = self.min_max(1, board, my_color, turn, en_passant, castling, None)
+        move = self.min_max(2, board, my_color, turn, en_passant, castling, None)
         print("ruch ai to:", move)
         return move
 
 
     def min_max(self, depht, board, my_color, turn, en_passant, castling, move): #not worikin yet
-        if depht == 0 or self.game_ended():
+        if depht == 0 or self.game_ended(board, my_color, castling, en_passant, turn):
+            move = (self.evaluating(board, self.all_pieces(board), my_color, castling, turn, en_passant), move[1]) #need to add evaluating score of mate and stalemate
             return move
 
-        if move != None:
-            print("ruch to " ,move)
             
         if my_color != turn: #posible ai move
             max_val = -999999999
@@ -26,35 +25,33 @@ class AI:
             for move_set in range(len(moves)):
                 for move in  moves[move_set][1]:
                     board_after_move = self.board_after_move(board, (moves[move_set][0], move))
-                    varible = (self.evaluating(board_after_move, self.all_pieces(board_after_move), my_color), (moves[move_set][0], move))
+                    varible = (self.evaluating(board_after_move, self.all_pieces(board_after_move), my_color, castling, turn, en_passant), (moves[move_set][0], move))
                     print("varible", varible)
-                    value = self.min_max(depht - 1, board_after_move, my_color, "white" if turn == "black" else "white", en_passant, castling, varible)
-                    valuee = self.evaluating(board_after_move, self.all_pieces(board_after_move), my_color)
+                    next_turn = "black" if turn == "white" else "white"
+                    value = self.min_max(depht - 1, board_after_move, my_color, next_turn, en_passant, castling, varible)
 
-                    if max_val < valuee:
+                    if value[0] > max_val:
+                        max_val = value[0]
                         best_move = (moves[move_set][0], move)
-                        print("best move: ", best_move)
-                    max_val = max(valuee, max_val)
                         
             max_val = (max_val, best_move)
             return max_val
         
         else: #posible player move
-            max_val = -999999999
+            min_val = 999999999
             pieces = self.all_pieces(board)
             moves = self.all_moves(board, pieces, turn, my_color, en_passant, castling)
             for move_set in range(len(moves)):
                 for move in  moves[move_set][1]:
                     board_after_move = self.board_after_move(board, (moves[move_set][0], move))
-                    varible = (self.evaluating(board_after_move, self.all_pieces(board_after_move), my_color), (moves[move_set][0], move))
+                    varible = (self.evaluating(board_after_move, self.all_pieces(board_after_move), my_color, castling, turn, en_passant), (moves[move_set][0], move))
                     print("varible", varible)
-                    value = self.min_max(depht - 1, board_after_move, my_color, "white" if turn == "black" else "white", en_passant, castling, varible)
-                    valuee = self.evaluating(board_after_move, self.all_pieces(board_after_move), my_color)
+                    next_turn = "black" if turn == "white" else "white"
+                    value = self.min_max(depht - 1, board_after_move, my_color, next_turn, en_passant, castling, varible)
                     
-                    if max_val < valuee:
+                    if min_val > value[0]:
                         best_move = (moves[move_set][0], move)
-                        print("best move: ", best_move)
-                    min_val = min(valuee, min_val)  
+                        min_val = value[0] 
             
             min_val = (min_val, best_move)
             return min_val
@@ -82,10 +79,10 @@ class AI:
                         moves.append(((r,c), chess_moves.rook_moves(board, r, c, turn, my_color)))
                     
                     elif piece.lower() == "q":
-                        moves.append(((r,c), chess_moves.king_moves(board, r, c, turn, castling, my_color)))
+                        moves.append(((r,c), chess_moves.queen_moves(board, r, c, turn, my_color)))
                     
                     elif piece.lower() == "k":
-                        moves.append(((r,c), chess_moves.queen_moves(board, r, c, turn, my_color)))
+                        moves.append(((r,c), chess_moves.king_moves(board, r, c, turn, castling, my_color)))
         return moves
                     
     
@@ -102,34 +99,40 @@ class AI:
         return pieces
     
      
-    def evaluating(self, board, pieces, my_color):
+    def evaluating(self, board, pieces, my_color, castling, turn, en_passant):
+        chess_moves = chess.piece_moves()
+        mate =  chess_moves.check_mate(board, my_color, castling, en_passant)
+        stalemate =  chess_moves.is_stalemate(board, my_color, castling, turn, en_passant)
+        if mate[0] or mate[1]:
+            if my_color == "black":
+                return -999 if mate[0] else 999
+            else:
+                return 999 if mate[1] else -999
+        
+        elif stalemate[0] or stalemate[1]:
+            return 0
+        
+        values = {
+            "p": 1,
+            "b": 3,
+            "n": 3,
+            "r": 5,
+            "q": 9,
+            "k": 0,
+        }
         white, black = 0, 0
         points = 0
+        
         for color in range(2):
             for r, c in pieces[color]:
-                if board[r][c].lower() == "p":
-                    points += 1
-                    
-                elif board[r][c].lower() == "b":
-                    points += 3
-                    
-                elif board[r][c].lower() == "n":
-                    points += 3
-                    
-                elif board[r][c].lower() == "r":
-                    points += 5
-                    
-                elif board[r][c].lower() == "q":
-                    points += 9
-                    
-                elif board[r][c].lower() == "k":
-                    points += 0
+                points += values[board[r][c].lower()]
                     
             if color == 0:
-                white += points
+                white = points
             else:
-                black += points
+                black = points
             points = 0
+            
         print(white,"  " , black, pieces)
         
         score = white - black if my_color == "black" else (white - black) * -1
@@ -137,9 +140,15 @@ class AI:
     
     
     
-    def game_ended(self):
+    def game_ended(self, board, my_color, castling, en_passant, turn):
+        chess_moves = chess.piece_moves()
+        mate =  chess_moves.check_mate(board, my_color, castling, en_passant)
+        stalemate =  chess_moves.is_stalemate(board, my_color, castling, turn, en_passant)
+        
+        if mate[0] or mate[1] or stalemate[0] or stalemate[1]:
+            return True
         return False
-        pass
+
     
     
     def board_after_move(self, board, move):
